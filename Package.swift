43c2fef8f365587465d32f71f417eadf67c5a484
Leonce Nshuti <nshutl0@sewanee.edu>
2023-06-07 07:18:40 -0500

    update package

--- Package.swift
+++ Package.swift
@@ -12,14 +12,10 @@ let package = Package(
             targets: ["GREAIBuddy"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // For example, a package might use a library for some functionality,
-        // a server for a component, a UI for its interface, etc.
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"), // For networking
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.4"), // For command-line parsing
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"), // For logging
         .package(url: "https://github.com/apple/swift-algorithms", from: "0.0.1"), // For additional Swift algorithms
-        // Add other dependencies as needed.
     ],
     targets: [
         .target(
@@ -29,10 +25,9 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "Algorithms", package: "swift-algorithms"),
-                // Add other target dependencies as needed.
             ]),
         .testTarget(
-            name: "GREAIBuddy",
+            name: "GREAIBuddyTests", // Renamed the test target
             dependencies: ["GREAIBuddy"]),
     ]
 )
