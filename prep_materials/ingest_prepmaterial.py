c173324106dcf0db76dc862f7e5f253f062a2421
Leonce Nshuti <nshutl0@sewanee.edu>
2023-06-07 20:30:50 -0500

    add python ingestion file

--- prep_materials/ingest_prepmaterial.py
+++ prep_materials/ingest_prepmaterial.py
@@ -1,50 +1,46 @@
-from llama_hub.file.base import SimpleDirectoryReader
-
-# other way of loading
-# from llama_index import download_loader
-# SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
-
-loader = SimpleDirectoryReader('./data', recursive=True, exclude_hidden=True)
-documents = loader.load_data()
-
-
-from llama_hub.file.base import SimpleDirectoryReader
-from llama_index import GPTVectorStoreIndex
-
-# other way of loading
-# from llama_index import download_loader
-# SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
-
-loader = SimpleDirectoryReader('./data', recursive=True, exclude_hidden=True)
-documents = loader.load_data()
-index = GPTVectorStoreIndex.from_documents(documents)
-index.query('What are these files about?')
-
+import streamlit as st
 from llama_hub.file.base import SimpleDirectoryReader
 from llama_index import GPTVectorStoreIndex
 from langchain.agents import initialize_agent, Tool
 from langchain.llms import OpenAI
 from langchain.chains.conversation.memory import ConversationBufferMemory
 
-# other way of loading
-# from llama_index import download_loader
-# SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
+def load_documents():
+    loader = SimpleDirectoryReader('./data', recursive=True, exclude_hidden=True)
+    documents = loader.load_data()
+    return documents
 
-loader = SimpleDirectoryReader('./data', recursive=True, exclude_hidden=True)
-documents = loader.load_data()
-index = GPTVectorStoreIndex.from_documents(documents)
+def index_documents(documents):
+    index = GPTVectorStoreIndex.from_documents(documents)
+    return index
 
-tools = [
-    Tool(
-        name="Local Directory Index",
-        func=lambda q: index.query(q),
-        description=f"Useful when you want answer questions about the files in your local directory.",
-    ),
-]
-llm = OpenAI(temperature=0)
-memory = ConversationBufferMemory(memory_key="chat_history")
-agent_chain = initialize_agent(
-    tools, llm, agent="zero-shot-react-description", memory=memory
-)
+def initialize():
+    documents = load_documents()
+    index = index_documents(documents)
+    tools = [
+        Tool(
+            name="Local Directory Index",
+            func=lambda q: index.query(q),
+            description=f"Useful when you want answer questions about the files in your local directory.",
+        ),
+    ]
+    llm = OpenAI(temperature=0)
+    memory = ConversationBufferMemory(memory_key="chat_history")
+    agent_chain = initialize_agent(
+        tools, llm, agent="zero-shot-react-description", memory=memory
+    )
 
-output = agent_chain.run(input="What are these files about?")
+    return agent_chain
+
+def app():
+    st.title('Local File Query Tool')
+    
+    agent_chain = initialize()
+
+    query = st.text_input("Enter your question about the files:")
+    if query:
+        output = agent_chain.run(input=query)
+        st.write(output)
+
+if __name__ == '__main__':
+    app()
